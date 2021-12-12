12077039cb7300b0e5fa24c837198d2b43954283
zethcameron <zethcameron@gmail.com>
2021-12-12 23:47:59 +1100

    a start

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,91 @@
 var randList1 = [
-	"t",
-	"y",
-	"u"
+	"Brent",
+	"Chelsea",
+	"Lauren",
+	"Meg",
+	"Chris",
+	"Andy",
+	"Nadja",
+	"Will",
+	"Nick",
+	"Sophia",
+	"Mike",
+	"Marcus",
+	"Ben",
+	"Charlotte",
+	"Max",
+	"Lexi",
+	"Emily",
+	"Caroline"
 ];
 
 var randList2 = [
-	"i",
-	"o",
-	"p"
+	"the ex-teacher",
+	"the business graduate",
+	"the snitch",
+	"the class traitor",
+	"the funny guy",
+	"the one who loves mindfulness",
+	"the high school bully",
+	"the absent type",
+	"the hype guy",
+	"the cool one",
+	"the down to earth listener",
+	"the mum friend",
+	"the wine enthusiast",
+	"the one who's always travelling",
+	"the liar",
+	"the one who also hates the system"
+	"the disability advocate"
 ];
 
 var randList3 = [
-	"2",
-	"xxx",
-	"xxx"
+	"hates you",
+	"good at finding jobs you are overqualified for",
+	"good at finding jobs you are underqualified for",
+	"sounds very far away during phone calls",
+	"still mispronounces your name",
+	"still can't remember your pronouns",
+	"laughs at your jokes",
+	"gossips about you to coworkers",
+	"thinks you'll be alright",
+	"ends appointments early",
+	"never ends appointments early",
+	"regularly ghosts you",
+	"makes your job plan a bit worse each time",
+	"heckling you for pay slips",
+	"pretending to care",
+	"outing you to future employers",
+	"has never sent you a job or training opportunity",
+	"takes credit for your successful job hunting"
+
 ];
 
 var randList4 = [
-	"xxx",
-	"xxx",
-	"xxx"
+	"whack",
+	"totally off",
+	"just not very nice",
+	"maybe hates you",
+	"nice but not sincere",
+	"this job is just a stepping stone for them",
+	"thinks they don't have any disabled friends",
+	"totally jaded",
+	"wishes they were doing anything else",
+	"bisexual but you still don't like them",
+	"funny but you can never be friends",
+	"somehow you pity this one",
+	"lawful evil",
+	"seems good with kids and no one else",
+	"probably has 100000 linkedin connections",
+	"used to be cool"
 ];
 
 var randList5 = [
-	"xxx",
-	"xxx",
-	"xxx"
+	"Bouldering",
+	"Going out on Smith Street",
+	"Watching the tennis",
+	"Rug-tufting",
+
 ];
 
 var randList6 = [
